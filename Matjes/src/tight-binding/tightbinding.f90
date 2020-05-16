f937d67fb3079a99baac008961850e2597c1ed99
Alexandre Heuchamps <aheuchamps@wellington.cm.cluster>
2020-05-16 11:37:27 +0200

    Associating a pointer to a lattice (tight-binding simulation)

--- Matjes/src/tight-binding/tightbinding.f90
+++ Matjes/src/tight-binding/tightbinding.f90
@@ -13,9 +13,12 @@
 
 
 subroutine tightbinding(my_lattice,my_motif,io_simu,ext_param)
+    use m_basic_types, only : vec_point
     use m_derived_types, only : cell,lattice,io_parameter,simulation_parameters
     use m_local_energy, only : get_E_matrix
-    use m_fftw, only : get_FFT_vec_point
+    use m_fftw
+    use m_lattice
+    use m_operator_pointer_utils
 
     implicit none
     ! internal parameter
@@ -24,7 +27,21 @@ subroutine tightbinding(my_lattice,my_motif,io_simu,ext_param)
     type(cell), intent(in) :: my_motif
     type(simulation_parameters), intent(in) :: ext_param
 
-    ! Here, the FFT of the Hamiltonian matrix will be computed.
-    ! First, load the matrix
+    ! N_cell is the variable that will contain the number of unit
+    ! cells in the simulation
+    integer :: N_cell
+    ! shape_lattice is an array of 4 integer that will contain the
+    ! diemnsions along x, y, and z, and the number of atoms per unit
+    ! cell (for exemple, 1 in BCC, 4 in FCC, etc.)
+    integer :: shape_lattice(4)
+    ! all_mode is an array of custom type vec_point that will
+    ! contain all the modes present in the simulation
+    type(vec_point),allocatable :: all_mode(:)
+    shape_lattice=shape(my_lattice%l_modes)
+    N_cell=product(shape_lattice)
 
+    allocate(all_mode(N_cell))
+    write(*,*) 'Coucou from file ', __FILE__, ' at line ', __LINE__
+
+    call associate_pointer(all_mode,my_lattice)
 end subroutine tightbinding
