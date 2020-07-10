9b6e57ebe84798cc61bfb9a57e2942d7945dc309
louise <louise.desplat@gmail.com>
2020-07-10 14:08:21 +0200

    recursive sparse matrix product

--- Matjes/src/Sparselib/eigen_sparse_reduce.cpp
+++ Matjes/src/Sparselib/eigen_sparse_reduce.cpp
@@ -1,4 +1,4 @@
-//compile (decomment main): g++ -std=c++0x  eigen_sparse_reduce.cpp
+//compile (decomment main): g++ -std=c++0x  -O3 eigen_sparse_reduce.cpp
 
 #include <iostream>
 #include <fstream>
@@ -7,6 +7,8 @@
 #include <stdlib.h>     /* srand, rand */
 #include <math.h>
 #include <vector>
+#include <time.h>       /* time */
+#include <chrono>
 #include <Eigen/Core>				
 #include <Eigen/SparseCore>
 #include <Eigen/Sparse>
@@ -16,11 +18,14 @@ using namespace Eigen;
 
 
 typedef SparseMatrix<double> SpMat; // declares a column-major sparse matrix type of doubles
+typedef SparseVector<double> SpVec; // declares a column-major sparse vector type of doubles
 typedef Triplet<double> Trplt; // declares triplet type of doubles
 typedef vector<double> oneline; //row vector of doubles
 typedef vector<oneline> vectoroflines; // vector of rows of doubles
 
 
+clock_t clock_start, clock_end;
+double exe_time;						//execution time											
 //_____________________________________________________________________________________|
 //										       |
 // 					prototypes				       |
@@ -31,13 +36,17 @@ typedef vector<oneline> vectoroflines; // vector of rows of doubles
 void build_sparse_matrix(vectoroflines &A_dense,
 			 SpMat &A_sparse);
 
+void build_sparse_vector(oneline &x_dense,
+			 SpVec &x_sparse);
+
 void reduce_A(const int n,
 	      int m_out,
-	      oneline &x_in,
+	      SpVec &x_sparse,
 	      SpMat &A_sparse_in,
 	      SpMat &A_sparse_out);
 
-void reduce_all(const int p, 
+void reduce_all(const int n,
+		const int p,
 		SpMat   &A_sparse_in,
 		oneline &x_in,
 		oneline &A_reduced);
@@ -52,23 +61,26 @@ void reduce_all(const int p,
 
 int main(){
 
-	const int n=3;
-	const int p=3;
+	const int n=9; //the hamiltonian has dimension n x n^p, this should be passed from Fortran
+	const int p=2;
 
-
-	// --- declare Eigen types--- //
-	 SpMat matrix_sparse_in;
+	// --- Eigen types--- //
+	 SpMat matrix_sparse_in(n,pow(n,p));
 
 	// ---- std vector types -----//
 
-	vectoroflines matrix_large; //hamiltonian
+	vectoroflines matrix_large; 				//hamiltonian
 	oneline row;
-	oneline M_int; //order parameter
-	oneline matrix_reduced; //reduced hamiltonian
+	oneline M_int;						//order parameter
+	oneline matrix_reduced; 				//reduced hamiltonian
 
+	// ---- fill test vectors -----//
+	cout << "Filling test matrix of dimensions " << n << " x " << pow(n,p) << " and vector of dim " << n << "..." << endl;
+	clock_start = clock();
 	for(int i=0;i<n;i++){
 		for(int j=0;j<pow(n,p);j++){
-			if(i==j){
+			if((i*j)%3==0){
+
 				row.push_back(i+j);
 			}
 			else
@@ -82,7 +94,7 @@ int main(){
 		M_int.push_back(i*i);
 	}
 
-	cout << "matrix_large =" << endl;
+	/*cout << "matrix_large =" << endl;
 	for(int i=0;i<n;i++){
 		for(int j=0;j<pow(n,p);j++){
 			cout << "\t" << matrix_large[i][j];
@@ -93,17 +105,24 @@ int main(){
 	cout << "M_int = " << endl;
 	for(int i=0;i<n;i++){
 		cout << M_int[i] << endl;
-	}
+	}*/
 
+
+	clock_end = clock();
+	exe_time = (clock_end - clock_start)/CLOCKS_PER_SEC;
+	cout << "Done, execution time = " << exe_time  << " s."<<endl;
 
 	// --- build sparse matrix --- //
+	cout << "Building sparse matrix..." << endl;
+	clock_start = clock();
 	build_sparse_matrix(matrix_large,matrix_sparse_in);
+	clock_end = clock();
+	exe_time = (clock_end - clock_start)/CLOCKS_PER_SEC;
+	cout << "Done, execution time = " << exe_time  << " s."<<endl;
 
 
 	// --- call reduce all --- //
-	reduce_all(p,matrix_sparse_in,M_int,matrix_reduced);
-
-	//cout << "matrix_out=" << endl << matrix_out << endl;
+	reduce_all(n,p,matrix_sparse_in,M_int,matrix_reduced);
 }
 
 
@@ -120,10 +139,10 @@ void build_sparse_matrix(vectoroflines &A_dense,
 
 	// ----------------- variables---------------------- //
 
-	vector<Trplt> tripletList; // std vector for triplet list: row index, column index, value.
-	//tripletList.reserve(10*m_in); //reserve room for non-zero elements (here 10/column)
-	const int n_1=A_dense.size(); //number of rows
-	const int n_2=A_dense[0].size(); //number of columns
+	vector<Trplt> tripletList; 				// std vector for triplet list: row index, column index, value.
+	const int n_1=A_dense.size(); 				//number of rows
+	const int n_2=A_dense[0].size(); 			//number of columns
+	//tripletList.reserve(10* n_2); 				//reserve room for non-zero elements (here 10/column)
 
 	// --- fill triplet list and fill sparse matrix A--- //
 
@@ -134,10 +153,39 @@ void build_sparse_matrix(vectoroflines &A_dense,
 			}
 		}	
 	}
-	//SpMat A_sparse(n_1,n_2); //declare sparse matrix of doubles, column-major by default
+	A_dense.clear();
 	A_sparse.setFromTriplets(tripletList.begin(), tripletList.end()); //use triplet to fill it
 }
 
+//_____________________________________________________________________________________|
+//										       |
+// 					Build sparse vector			       |
+//										       |				      
+//_____________________________________________________________________________________|
+
+void build_sparse_vector(oneline &x_dense,
+			 SpVec &x_sparse){
+
+	// ----------------- variables---------------------- //
+
+
+	const int n=x_dense.size(); //number of rows
+	x_sparse.reserve(n);
+	//vector<Trplt> tripletList; // std vector for triplet list: row index, column index, value
+
+	//tripletList.reserve(n); //reserve room for non-zero elements (here 10/column)
+
+	// --- fill triplet list and fill sparse matrix x--- //
+
+	for(int i=0;i<n;i++){
+		if (x_dense[i]!=0.0){ 
+	  		x_sparse.insert(i) = x_dense[i];		
+		}	
+	}
+	x_dense.clear();
+	//x_sparse.makeCompressed();   
+	//x_sparse.setFromTriplets(tripletList.begin(), tripletList.end()); //use triplet to fill it
+}
 //_____________________________________________________________________________________|
 //										       |
 // 					reduce A with eigen			       |
@@ -147,31 +195,24 @@ void build_sparse_matrix(vectoroflines &A_dense,
  //function that uses Eigen to multiply sparse matrix A of size (n,n^p) by dense vector x of size n
 void reduce_A(const int n,
 	      int m_out,
-	      oneline &x_in,
+	      SpVec &x_sparse,
 	      SpMat &A_sparse_in,
 	      SpMat &A_sparse_out){
 
-	//map std vector to eigen type
-    	VectorXd x =Map< Matrix<double, Dynamic, 1> >(x_in.data());
-	//SpMat x= Map<SpMat,Dynamic,1>(x_in.data());
-  	// Map<MatrixXd> temp(A_reduced.data(), n,m_out);
-	// --- get reduced matrix A*x ---//
-	//VectorXd A_reduced(m_out*n,1); //store in dense matrix 
-	//SpMat A_reduced(m_out*n,1); //store in sparse matrix 	
-	A_sparse_out=(A_sparse_in.transpose() * x).pruned(); 
-	//A_sparse_out.prune();
+	// --- perform product ---//
+	A_sparse_out=(A_sparse_in.transpose() * x_sparse).pruned();
 
-//sm3 = (sm1 * sm2).pruned();                  // product of sparse matrices removes numerical zeros
-//mat.prune()
+	// --- reshape into an n,n^{p-1} via dense matrix class---//
+	MatrixXd dMat;
+	dMat = MatrixXd(A_sparse_out);
+	dMat.resize(n,m_out);
+	//cout << "dMat="<<endl<<dMat<<endl;
+	A_sparse_out=dMat.sparseView();
 
- 	cout <<"reduced matrix has " A_sparse_out.nonZeros() << " non-zero elements, "<<  A_sparse_out.rows() << " row(s) and " << A_sparse_out.cols() << " column(s)." << endl;
+	//cout << "in reduce_A, after resizing temp=" << endl << MatrixXd(A_sparse_out) << endl;
 
-	// --- reshape into an n,n^{p-1}, ! might need to transpose or not ---//
-	//SparseMatrix<double> A11;
-   	 A_sparse_out.resize(n,m_out);
+ 	cout <<"reduced matrix has "<<  A_sparse_out.nonZeros() << " non-zero elements, "<<  A_sparse_out.rows() << " row(s) and " << A_sparse_out.cols() << " column(s)." << endl;
 
-	//Map<MatrixXd> temp(A_reduced.data(), n,m_out);
-	//A_out=temp.transpose();
 
 }
 
@@ -182,31 +223,43 @@ void reduce_A(const int n,
 //_____________________________________________________________________________________|
 // takes sparse matrix A and calls reduce_A recursively by doing A=A*x until A is a vector
 
-void reduce_all(const int p, //rank of A -1? so that A is of size n x n^p 
+void reduce_all(const int n,
+		const int p, //rank of A -1? so that A is of size n x n^p 
 		SpMat   &A_sparse_in,
 		oneline &x_in,
 		oneline &A_reduced){
 
-	const int n=x_in.size(); 
-	//const int m_in=A_dense[0].size(); //number of columns in A	
-	int m_out=pow(n,p-1); //number of columns in A after reduction
 
-	//SpMat A_sparse_in;
-	SpMat A_sparse_out; 
-	//MatrixXd A_dense_out(dynamical?); //output of reduce_A, size should be dynamical
+	int m_out=pow(n,p-1); 				//number of columns in A after reduction
+	SpVec x_sparse(n); 				//sparse vector for order parameter
+	SpMat A_sparse_out_1(n,m_out); 			//after one reduction
+	SpMat A_sparse_out_2(n,pow(n,p-2)); 		//after two reductions
+	MatrixXd A_dense_out;
 
 
-	//reduce A_sparse until it is a vector
-	//while(m_out>1){
-		//reduce_A(n,m_out,A_sparse_in,x_in,A_sparse_out);
-	reduce_A(n,m_out,x_in,A_sparse_in,A_sparse_out);
+	//build sparse vector from x
+	cout << "Building sparse vector..." << endl;
+	clock_start = clock();
+	build_sparse_vector(x_in,x_sparse);
+	//cout << "x_sparse=" << endl << VectorXd(x_sparse) << endl;
+	clock_end = clock();
+	exe_time = (clock_end - clock_start)/CLOCKS_PER_SEC;
+	cout << "Done, execution time = " << exe_time  << " s."<<endl;
 
-	//	m_out=A_dense_out.cols()
+	// reduce recursively
+	cout << "Beginning reduction..." << endl;
+	clock_start = clock();
+	reduce_A(n,m_out,x_sparse,A_sparse_in,A_sparse_out_1);
+	m_out=pow(n,p-2);
+	reduce_A(n,m_out,x_sparse,A_sparse_out_1,A_sparse_out_2);
 
-		//then something like A_sparse = A_dense_out 
-
-	//}
-
+	//convert back to dense view
+	A_dense_out= MatrixXd(A_sparse_out_2);
+	clock_end = clock();
+	exe_time = (clock_end - clock_start)/CLOCKS_PER_SEC;
+	cout << "Done, execution time = " << exe_time  << " s."<<endl;
+	//cout << "A_dense_out=" << endl << A_dense_out << endl;
+	cout << "A_dense_out has" << endl <<A_dense_out.rows() << " rows and " <<  A_dense_out.cols() << " column(s). " << endl;
 
 }
 
