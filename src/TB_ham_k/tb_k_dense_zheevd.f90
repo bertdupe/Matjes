31d03ba284a58d74703a6c548dfc2a06d8f2ccc0
Patrick Buhl <pbuhl@uni-mainz.de>
2021-08-17 14:11:12 +0200

    minor fix zheevd

--- src/TB_ham_k/tb_k_dense_zheevd.f90
+++ src/TB_ham_k/tb_k_dense_zheevd.f90
@@ -86,13 +86,13 @@ subroutine calc_evec(this,Nin,eval,evec,Nout,work)
                 & work%cmplx_arr, lcwork, work%real_arr, lrwork, work%int_arr, liwork, info)
 
     if(info/=0) ERROR STOP "LAPACK ERROR"
+    Nout=this%dimH
     if(Nout>Nin)then
         write(error_unit,'(/A)') "Trying to diagonalize Hamiltonian with zheevd, but output Hamiltonian size has been set to low (TB_diag_estNe)"
         write(error_unit,'(AI8)') "Found eigenvalues in interval:", Nout
         write(error_unit,'(AI8)') "Assumend maximal array size  :", Nin
         ERROR STOP "INCREASE ARRAY SIZE (TB_diag_estNe) OR DECREASE ENERGY WINDOW TB_diag_Ebnd."
     endif
-    Nout=this%dimH
     evec=this%H
 end subroutine
 
