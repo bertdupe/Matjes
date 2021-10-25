481dd09b8435e4d1dc41c64589d13c709583b073
Bertrand Dupé <bertdupe@users.noreply.github.com>
2021-10-25 17:59:24 +0200

    Create general exchange
    starting a new stuff

---
+++ src/Energy/Exchange_Heisenberg_general.f90
@@ -0,0 +1,8 @@
+module m_exchange_heisenberg_general
+use m_input_H_types, only: io_H_ExchG
+use, intrinsic :: iso_fortran_env, only : output_unit
+implicit none
+private
+public read_ExchG_input, get_exchange_ExchG, get_exchange_ExchG_fft
+
+end module
