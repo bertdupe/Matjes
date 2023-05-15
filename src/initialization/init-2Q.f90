b2afc5dec13d275801e53b5232a0873e24f6bf74
louise <louise.desplat@gmail.com>
2023-05-15 15:20:08 +0200

    clean up init-2Q

--- src/initialization/init-2Q.f90
+++ src/initialization/init-2Q.f90
@@ -42,25 +42,9 @@ subroutine init_2Q(io,fname,lat,ordname,dim_mode,state,init_conf)
 
     call get_parameter(io,fname,'Q1_'//ordname,3,q1)
     call get_parameter(io,fname,'Q2_'//ordname,3,q2)
-    call get_parameter(io,fname,'Qnorm_'//ordname,qnorm)
-    !if(norm2(q1)==0.0d0) ERROR STOP "Q1 has to differ from 0"
-    !if(norm2(q2)==0.0d0) ERROR STOP "Q2 has to differ from 0"
-    !write(*,*)'Warning: Q1 and Q2 should be normalized by the user for Qnorm to be their actual norm.'
-    !if(qnorm>0.0d0)then
-  
-    !else
-    !    q1(:)=0.0d0
-    !    q2(:)=0.0d0
-    !endif
-    !write(*,*)'before a* matrix mult: Q1=',q1,' Q2=',q2
-    !write(*,*)'lat%astar=',lat%astar
     q1=matmul(q1,lat%astar)
     q2=matmul(q2,lat%astar)
- 	 !q1=q1*qnorm
-    !q2=q2*qnorm
 
-
-	
     qk=q1 !(q1+q2)*0.5d0
     qm=q2 !(q1-q2)*0.5d0
 	write(*,*)'Q1=',q1,' Q2=',q2,' QM=',qm,' QK=', qk
@@ -71,22 +55,13 @@ subroutine init_2Q(io,fname,lat,ordname,dim_mode,state,init_conf)
     
 	 write(*,*)'Warning: mx and my have been swapped in 2Q state formula. my=sin(phi)'
     do i=1,Nsite
-
         phi  =dot_product(qm,pos_3(:,i))
         theta=dot_product(qk,pos_3(:,i))
-
         state_3(1,i)=cos(phi)*sin(theta)
         state_3(2,i)=sin(phi)
         state_3(3,i)=cos(phi)*cos(theta)
-                 ! write(*,*)'pos_3(:,i)=',pos_3(:,i)
-            	!	write(*,*)'phi=',phi
-            		!write(*,*)'theta=',theta
-            		!write(*,*)'mx=', state_3(1,i)
-            		!write(*,*)'my=', state_3(2,i)
-            		!write(*,*)'mz=', state_3(3,i)
         j=mod(i-1,size_unit_cell)+1
         state_3(:,i)=state_3(:,i)*init_conf(j)/abs(init_conf(j))
-       ! write(*,*)'then state_3(:,i)=',state_3(:,i)
     enddo
 
     nullify(pos_3,state_3)
